f528b1aacf7690195d0f189f3cfa6b98ce580256
AnandPawanipagar <85126312+AnandPawanipagar@users.noreply.github.com>
2021-11-07 17:10:57 +0530

    basic link tag is used

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.scss";
-import { useState } from "react";
+
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Grid, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
@@ -9,26 +10,31 @@ const ConsultentButton = styled(Button)({
   backgroundColor: "#ec5b53",
   color: "white",
 });
-const tabs = ["Home", "About", "Services", "Portfolio", "Page", "Contact"];
+const tabs = [
+  { tabName: "Home", address: "/" },
+  { tabName: "About", address: "/about" },
+  { tabName: "Services", address: "/services" },
+  { tabName: "Page", address: "/page" },
+  { tabName: "Contact", address: "/contact" },
+];
 
 const Navbar = () => {
   return (
     <>
       <Grid container spacing={0} className="navbarContainer">
         <Grid xs={4} className="navbarContainer--logoGrid">
-          <img
-            src="images/logo.webp"
-            alt="Portfolio"
-          />
+          <img src="images/logo.webp" alt="Portfolio" />
         </Grid>
         <Grid className="navbarContainer--tabsGrid" xs={8}>
           {tabs.map((tab) => {
             return (
-              <Button
-                className="navbarContainer--tabsGrid--tabs"
-                variant="text"
-              >
-                {tab}
+              <Button variant="text">
+                <Link
+                  className="navbarContainer--tabsGrid--tabs"
+                  to={tab.address}
+                >
+                  {tab.tabName}
+                </Link>
               </Button>
             );
           })}
